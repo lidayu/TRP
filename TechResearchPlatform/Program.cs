baef73d3a5d9ac4753bbfb0cb7699c5c03666349
Doy@Com <lidayu@abchina.com>
2014-11-29 15:45:38 +0800

    new project,no meaning

---
+++ TechResearchPlatform/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace TechResearchPlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("lidayu", "lidayu@abchina.com");
+            Console.WriteLine(ht["lidayu"]);
+            Console.ReadKey();
+
+        }
+    }
+}
